7cc27e2df4bf10243017bd3b5bcf62e9c5687550
waterfallbotowner <admin@waterfallbot.info>
2021-07-01 16:36:48 +0000

    add token page

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3cf7b8693effcd46a6cfd00cfb0939ff.js"
+  "/precache-manifest.a410cf9b3267402ad4312a33825bd254.js"
 );
 
 self.addEventListener('message', (event) => {
