ffa7fce1e586ac3cb37199346d8539513e3bb7db
waterfallbotowner <admin@waterfallbot.info>
2021-08-17 01:07:13 +0000

    update token Tue Aug 17 01:07:13 UTC 2021

--- precache-manifest.1a69460f0a2f82203726887581ce1697.js
+++ precache-manifest.1a69460f0a2f82203726887581ce1697.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d3f0631bbf39b916d3bad047d3bcda5c",
+    "revision": "7a4161a02535fbeecc182f4865817fca",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.15673bd2.chunk.css"
   },
   {
-    "revision": "b50899764552e8de3efb",
+    "revision": "fdf0607126c64208a812",
     "url": "/static/css/main.207237f2.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.04872022.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b50899764552e8de3efb",
-    "url": "/static/js/main.a59cd5b9.chunk.js"
+    "revision": "fdf0607126c64208a812",
+    "url": "/static/js/main.e069fa73.chunk.js"
   },
   {
     "revision": "15fcaf64f63611be9d52",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4a218499b82159e212910c977e9b3200.js"
+  "/precache-manifest.1a69460f0a2f82203726887581ce1697.js"
 );
 
 self.addEventListener('message', (event) => {
