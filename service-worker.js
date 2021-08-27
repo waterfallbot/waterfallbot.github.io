2d5bdcab9c0159a44ff7ff3c59e662e651f3d1cc
waterfallbotowner <admin@waterfallbot.info>
2021-08-27 13:58:21 +0000

    update token Fri Aug 27 13:58:21 UTC 2021

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37c2ea75f056224731aead1f289cefbf.js"
+  "/precache-manifest.cea9538046b23dd467b8fee871d4f70f.js"
 );
 
 self.addEventListener('message', (event) => {
